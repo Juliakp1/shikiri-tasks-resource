c23face0b50628f2373bb1ba28039e03b9b0fd6b
FeMCDias <femcdias@gmail.com>
2024-04-10 11:30:25 -0300

    fix pkey

--- src/main/resources/db/migration/V2024.04.08.002__create_table_tasks.sql
+++ src/main/resources/db/migration/V2024.04.08.002__create_table_tasks.sql
@@ -6,5 +6,5 @@ CREATE TABLE tasks
     ts_done character varying(2) NOT NULL,
     ts_tool character varying(36) NOT NULL,
     ts_board character varying(36) NOT NULL,
-    CONSTRAINT task_pkey PRIMARY KEY (id_task)
+    CONSTRAINT tasks_pkey PRIMARY KEY (id_task)
 );
